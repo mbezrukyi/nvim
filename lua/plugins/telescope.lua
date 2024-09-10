ea3b675ab054c8f35adce6ceee54b1bb0ab97dd6
Mykola Bezrukyi <kolya.bezrukiy.03@gmail.com>
2024-09-10 20:39:51 +0300

    update telescope.lua key mapping func

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,8 +7,20 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>pf', function() builtin.find_files({hidden = true, no_ignore = true}) end, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', builtin.grep_string, {})
+        vim.keymap.set('n', '<leader>pf', function() 
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true
+            })
+        end, {})
+        vim.keymap.set('n', '<leader>ps', function () 
+            builtin.grep_string({
+                search = vim.fn.input('Grep > '),
+                additional_args = {
+                    '--hidden',
+                    '--no-ignore',
+                },
+            }) 
+        end, {})
     end,
 }
