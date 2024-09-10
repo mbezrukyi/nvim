e28b1b22026210bf00e24d9c59b97771fd7e3e9c
Mykola Bezrukyi <kolya.bezrukiy.03@gmail.com>
2024-09-10 19:09:29 +0300

    reconfigure cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,21 +1,25 @@
 return {
     'hrsh7th/nvim-cmp',
-    opts = {
-        sources = {
-            {name = 'nvim_lsp'},
-        },
-        mapping = require('cmp').mapping.preset.insert({
-            ['<C-p>'] = require('cmp').mapping.select_prev_item({behavior = 'select'}),
-            ['<C-n>'] = require('cmp').mapping.select_next_item({behavior = 'select'}),
-            ['<CR>'] = require('cmp').mapping.confirm({select = false}),
-            ['<C-Space>'] = require('cmp').mapping.complete(),
-            ['<C-u>'] = require('cmp').mapping.scroll_docs(-4),
-            ['<C-d>'] = require('cmp').mapping.scroll_docs(4),
-        }),
-        snippet = {
-            expand = function(args)
-                vim.snippet.expand(args.body)
-            end,
-        },
-    },
+    config = function()
+        local cmp = require('cmp')
+
+        cmp.setup({
+            sources = {
+                {name = 'nvim_lsp'},
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+                ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+                ['<CR>'] = cmp.mapping.confirm({select = false}),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            }),
+            snippet = {
+                expand = function(args)
+                    vim.snippet.expand(args.body)
+                end,
+            },
+        })
+    end,
 }
