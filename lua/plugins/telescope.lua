1da876539607692a9c9a28def90dc402fc175045
Mykola Bezrukyi <kolya.bezrukiy.03@gmail.com>
2024-09-10 03:19:54 +0300

    update telescope.lua code

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
         'nvim-lua/plenary.nvim',
     },
-	keys = {
-		{"<leader>pf", require('telescope.builtin').find_files, desc = "Explore Files"},
-		{"<C-p>", require('telescope.builtin').git_files, desc = "Explore Git Files"},
-		{"<leader>ps", require('telescope.builtin').grep_string, desc = "Explore Files by String"},
-	},
+    keys = {
+        {"<leader>pf", require('telescope.builtin').find_files, desc = "Explore Files"},
+        {"<C-p>", require('telescope.builtin').git_files, desc = "Explore Git Files"},
+        {"<leader>ps", require('telescope.builtin').grep_string, desc = "Explore Files by String"},
+    },
 }
