6f7649ce335535314a1f0d0ad68131179195d1d2
mbezrukyi <kolya.bezrukiy.03@gmail.com>
2025-02-16 19:50:36 +0200

    move lsp configuration in one place

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,45 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
-    priority = 1000,
-    dependencies = { 
-        'neovim/nvim-lspconfig',
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
         'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
-        local lsp_zero = require('lsp-zero')
-
-        local lsp_attach = function(client, bufnr)
-            lsp_zero.default_keymaps({buffer = bufnr})
-        end
-
-        lsp_zero.extend_lspconfig({
-            sign_text = true,
-            lsp_attach = lsp_attach,
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        require('mason').setup()
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                'pylsp',
+            },
+            handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup({
+                        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                    })
+                end
+            }
         })
-    end,
+
+        local cmp = require('cmp')
+
+        cmp.setup({
+            sources = {
+                {name = 'nvim_lsp'},
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+                ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+                ['<CR>'] = cmp.mapping.confirm({select = false}),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            }),
+            snippet = {
+                expand = function(args)
+                    vim.snippet.expand(args.body)
+                end,
+            },
+        })
+    end
 }
