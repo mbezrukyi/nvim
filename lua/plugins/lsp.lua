8d8c19b612640fb3bf8afe4e3d9fb49d39239f99
Mykola Bezrukyi <kolya.bezrukiy.03@gmail.com>
2025-05-07 16:11:12 +0300

    update mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,7 @@ return {
     config = function()
         require('mason').setup()
         require('mason-lspconfig').setup({
+            automatic_enable = false,
             ensure_installed = {
                 'pylsp',
             },
